98af37b3a2f1904f000be227a02dba9df8415877
Ernesto Bossi <bossi.ernestog@gmail.com>
2021-06-15 17:42:31 -0300

    machete...

--- 0_ground_zero/third_container.sh
+++ 0_ground_zero/third_container.sh
@@ -1,2 +1,2 @@
-docker run --name jack_container -d ubuntu /bin/sh -c "while
+docker run --restart=always --name jack_container -d ubuntu /bin/sh -c "while
 true; do echo All work and no play makes Jack a dull boy; sleep 1; done"
